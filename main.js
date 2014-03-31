6d084aedbf30484384707f163919bab777de0691
Michael Little <bio595@gmail.com>
2014-03-31 18:30:34 +1300

    drew some sick ass lines

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 //Get the chart
 var chart = d3.select(".chart");
 
-var nums = [1, 2, 3, 4, 9];
+var points = [
+{x: 95, y: 13},
+{x: 73, y: 63},
+{x: 45, y: 13},
+{x: 45, y: 63},
+{x: 15, y: 23},
+];
+
 
 //Set it's width
 chart.attr("width", 800);
 chart.attr("height", 450);
 
-
-chart.selectAll("circle").data(nums).enter().append("circle")
+//Draw some circles
+chart.selectAll("circle").data(points).enter().append("circle")
 		.attr("r", 20)
-		.attr("cy", 100)
-		.attr("cx", function(d){return d* 50});
+		.attr("cy", function(p){return p.y * 5;})
+		.attr("cx", function(p){return p.x * 5;});
+
+
+//Draw lines between the circles
+point_pairs = [];
+for (var i = 0; i < points.length; i++) {
+	for (var j = 0; j < points.length; j++) {
+		if(points[i] !== points[j])
+			point_pairs.push([points[i], points[j]])
+	};
+};
+
+chart.selectAll("line").data(point_pairs).enter().append("line")
+	.attr("x1", function(pp){return pp[0].x * 5;})
+	.attr("y1", function(pp){return pp[0].y * 5;})
+	.attr("x2", function(pp){return pp[1].x * 5;})
+	.attr("y2", function(pp){return pp[1].y * 5;});
